bed484fc49cfe58af6e90d45ee3bded8ffa36bc5
Hubert Quarshie <hugheyq@gmail.com>
2021-12-10 10:50:12 +0000

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('form');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	const location = weatherForm.searchTerm.value;
+
+	const URI = `http://localhost:3000/weather?address=${location}`;
+
+	messageOne.textContent = 'Loading...';
+	messageTwo.textContent = '';
+
+	fetch(URI).then(response => {
+		response.json().then(data => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+	});
+});
